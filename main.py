32f2e2aa78a6503d2b1f4f5d292f0e5df20561a0
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-06-23 16:47:14 -0400

    made grid size expandable

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def make_grid():
+    n = int(input('Enter grid size: '))
+    while n <= 0:
+        n = int(input('Enter grid size: '))
+    grid = []
+    for x in range(n):
+        row = []
+        for y in range(n):
+            row.append('-')
+        grid.append(row)
+    return grid, n
 
-# display grid
 def display_grid(grid):
     for row in grid:
         print(row)
@@ -18,14 +28,14 @@ def get_input(grid):
         except:
             print("Please enter a valid number")
 
-def check_pos(row, col):
-    if (row < 0 or row > 2) or (col < 0 or col > 2):
+def check_pos(row, col, size):
+    if (row < 0 or row > size-1) or (col < 0 or col > size-1):
         return False
     return True
 
-def get_count(grid, new_row, new_col, symbol, case):
+def get_count(grid, new_row, new_col, symbol, case, size):
     count = 0
-    while(check_pos(new_row, new_col)):
+    while(check_pos(new_row, new_col, size)):
         if grid[new_row][new_col] == symbol:
             count += 1
         if case == 'down': new_row += 1
@@ -47,53 +57,58 @@ def get_count(grid, new_row, new_col, symbol, case):
         
     return count
 
-def check(grid, symbol, row, col):
+def check(grid, symbol, row, col, size):
     # on curr symbol, so count = 1
     count = 1
 
     # check vertically
     # going down
-    count += get_count(grid, row+1, col, symbol, 'down')
-    if count == 3: return True
+    count += get_count(grid, row+1, col, symbol, 'down', size)
+    if count == size: return True
+    print('vertically', count)
     # going up 
-    count += get_count(grid, row-1, col, symbol, 'up')
-    if count == 3: return True
+    count += get_count(grid, row-1, col, symbol, 'up', size)
+    if count == size: return True
     print('vertically', count)
     count = 1
 
     # check horizontally
     # going left
-    count += get_count(grid, row, col-1, symbol, 'left')
-    if count == 3: return True
+    count += get_count(grid, row, col-1, symbol, 'left', size)
+    if count == size: return True
+    print('horizontally', count)
     # going right
-    count += get_count(grid, row, col+1, symbol, 'right')
-    if count == 3: return True
+    count += get_count(grid, row, col+1, symbol, 'right', size)
+    if count == size: return True
     print('horizontally', count)
     count = 1
 
     # check slope down diagonal 
     # going left up diagonal
-    count += get_count(grid, row-1, col-1, symbol, 'diag1-left')
-    if count == 3: return True
+    count += get_count(grid, row-1, col-1, symbol, 'diag1-left', size)
+    if count == size: return True
+    print('diag1', count)
     # going right down diagonal
-    count += get_count(grid, row+1, col+1, symbol, 'diag1-right')
-    if count == 3: return True
+    count += get_count(grid, row+1, col+1, symbol, 'diag1-right', size)
+    if count == size: return True
     print('diag1', count)
     count = 1
 
     # check slope up diagonal 
     # going left down diagonal
-    count += get_count(grid, row+1, col-1, symbol, 'diag2-left')
-    if count == 3: return True
-    # going right down diagonal
-    count += get_count(grid, row-1, col+1, symbol, 'diag2-right')
-    if count == 3: return True
+    count += get_count(grid, row+1, col-1, symbol, 'diag2-left', size)
+    if count == size: return True
+    print('diag2', count)
+    # going right up diagonal
+    count += get_count(grid, row-1, col+1, symbol, 'diag2-right', size)
+    if count == size: return True
     print('diag2', count)
 
     return False
 
-def start(grid):
+def start():
     player = 'player1'
+    grid, size = make_grid()
     while(True):
         display_grid(grid)
         print("Your turn,", player)
@@ -104,7 +119,7 @@ def start(grid):
             symbol = 'o'
         grid[row][col] = symbol
         # check to see if there's a winner 
-        if check(grid, symbol, row, col):
+        if check(grid, symbol, row, col, size):
             print(player, 'wins')
             display_grid(grid)
             break
@@ -114,12 +129,4 @@ def start(grid):
         else:
             player = 'player1'
 
-grid = []
-
-for x in range(3):
-    row = []
-    for y in range(3):
-        row.append('-')
-    grid.append(row)
-
-start(grid)
+start()
